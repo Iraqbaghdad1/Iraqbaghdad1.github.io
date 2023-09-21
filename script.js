f385e0b8d08b26143a463d317f1711daf94b8f60
Iraqbaghdad1 <134105675+Iraqbaghdad1@users.noreply.github.com>
2023-09-21 09:30:06 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Example: Make an HTTP GET request to fetch data from your server
+  const fetchDataButton = document.getElementById('fetchData');
+  const responseDataDiv = document.getElementById('responseData');
+
+  fetchDataButton.addEventListener('click', () => {
+    // Make an HTTP GET request to fetch data from the server
     fetch('/api/some-data')
-        .then(response => response.json())
-        .then(data => {
-            // Handle the data received from the server
-            console.log(data);
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
+      .then(response => response.json())
+      .then(data => {
+        // Display the data received from the server
+        responseDataDiv.innerHTML = `<p>${data.message}</p>`;
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  });
 });
+
