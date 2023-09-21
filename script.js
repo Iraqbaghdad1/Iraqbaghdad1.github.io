c218a4789205d25b01f3265877d97f2b89566b02
Iraqbaghdad1 <134105675+Iraqbaghdad1@users.noreply.github.com>
2023-09-21 09:32:15 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const fetchDataButton = document.getElementById('fetchData');
-  const responseDataDiv = document.getElementById('responseData');
+  const contentContainer = document.getElementById('content');
 
-  fetchDataButton.addEventListener('click', () => {
-    // Make an HTTP GET request to fetch data from the server
-    fetch('/api/some-data')
-      .then(response => response.json())
-      .then(data => {
-        // Display the data received from the server
-        responseDataDiv.innerHTML = `<p>${data.message}</p>`;
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      });
+  // Handle navigation links
+  document.querySelectorAll('a').forEach(link => {
+    link.addEventListener('click', function(e) {
+      e.preventDefault(); // Prevent the default link behavior
+      
+      // Get the href attribute of the clicked link
+      const pageUrl = this.getAttribute('href');
+
+      // Fetch the content of the clicked page
+      fetch(pageUrl)
+        .then(response => response.text())
+        .then(content => {
+          // Update the content container with the fetched HTML
+          contentContainer.innerHTML = content;
+        })
+        .catch(error => {
+          console.error('Error:', error);
+        });
+
+      // Update the browser's URL
+      history.pushState(null, null, pageUrl);
+    });
   });
 });
-
